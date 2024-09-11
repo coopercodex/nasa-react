47037a5b0301ff7c4e7e809173bb17b284a3342d
Derek Cooper <derekcooper20@gmail.com>
2024-09-10 22:31:26 -0600

    adds styles

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <div className='sidebar'>
+      <div className='bgOverlay'> </div>
+      <div className="sidebarContents">
+      <h2>The Brutal Martian Landscape</h2>
+      <div>
+      <p>Description</p>
+      <p>asdfasdf</p>
+      <p>asdfasd</p>
+      </div>
+      <button><i className="fa-solid fa-arrow-right"></i></button>
+      </div>
+    </div>
   )
 }
 
