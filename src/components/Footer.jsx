1a617a9a473dd5b583bd93a5521ecae9157ad8a3
Derek Cooper <derekcooper20@gmail.com>
2024-09-11 22:26:22 -0600

    adds styles2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
-const Footer = () => {
+const Footer = (props) => {
+  const {showModal, handleToggleModal} = props
+
   return (
     <footer>
       <div className='bgGradient'></div>
     <div>
       <h2>The Martian Landscape</h2>
-      <h1>The APOD Project</h1>
+      <h1>The APOD Project</h1> 
     </div>
-    <button>
+    <button onClick={handleToggleModal}>
     <i class="fa-solid fa-circle-info"></i>
     </button>
   </footer>
